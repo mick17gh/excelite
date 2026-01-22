b27750a5117756ece47681cb9e48a70a3c906059
Michael Asante <mike17gh@gmail.com>
2026-01-22 17:26:29 +0000

    Bundle prisma engine safely

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,42 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'flexibiz.sfo3.cdn.digitaloceanspaces.com',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'flexibiz.sfo3.cdn.digitaloceanspaces.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "flexibiz.sfo3.cdn.digitaloceanspaces.com",
+        port: "",
+        pathname: "/**",
       },
     ],
+  },
+
+  experimental: {
+    // @ts-expect-error – supported by Next.js runtime but missing in types
+    outputFileTracingIncludes: {
+      "/*": ["lib/generated/prisma/**/*"],
+    },
   },
 };
 
 export default nextConfig;
+
+
